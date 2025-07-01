67e6add5752493cd551e982afb680d4a5516fa64
maxper4 <maxenceperion@gmail.com>
2025-07-01 17:34:22 +0200

    Add before prune reconciliation

--- src/lib.rs
+++ src/lib.rs
@@ -26,11 +26,19 @@ pub async fn run() {
     let (to_network_chan, from_network_chan, network_task) = network::run(&config).await;
 
     #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
-    struct Transaction {}
+    struct Transaction {
+        id: usize
+    }
+
+    impl Transaction {
+        pub fn new(id: usize) -> Self {
+            Transaction { id }
+        }
+    }
 
     impl Default for Transaction {
         fn default() -> Self {
-            Transaction {}
+            Transaction { id: 0}
         }
     }
 
@@ -80,24 +88,46 @@ pub async fn run() {
 
     fn mutate_mempool(state: &Vec<Transaction>, op: &Operation<MempoolParameter>) -> Vec<Transaction> {
         let mut state = state.clone();
-        match op.params {
-            MempoolParameter::Add(ref tx) => {
-                state.push(tx.clone());
-            },
-            MempoolParameter::Prune() => {
-                let mut result = vec![];
-                for _ in 0..BLOCK_SIZE {
-                    result.push(state.remove(0));
+        match op.id {
+            0 => {
+                match op.params {
+                    MempoolParameter::Add(ref tx) => {
+                        state.push(tx.clone());
+                    },
+                    _ => {}
                 }
-                println!("New block: {:?}", result);
-            }
+            },
+            1 => {
+                match op.params {
+                    MempoolParameter::Prune() => {
+                        let mut result = vec![];
+                        for _ in 0..BLOCK_SIZE {
+                            result.push(state.remove(0));
+                        }
+                        println!("New block: {:?}", result);
+                    },
+                    _ => {}
+                }
+            },
+            _ => {}
         }
+        
         state
     }
 
+    fn mempool_order(v1: &Vertex<VertexLabel<MempoolParameter>>, v2: &Vertex<VertexLabel<MempoolParameter>>) -> Ordering {
+        match (v1.label.op.id, v2.label.op.id) {
+            (0, 1) => Ordering::Less,  // add before prune
+            (1, 0) => Ordering::Greater, // prune after add
+            _ => v1.id.process_id.cmp(&v2.id.process_id) // same operation id
+        }
+    }
+
+    order_based_reconciliation!(Vec<Transaction>, MempoolParameter, mempool_order, mempool_reconciliation);
+
     mutate_if_legal!(Vec<Transaction>, MempoolParameter, mutate, mutate_mempool, mempool_legality);
 
-    let mempool = CRDT::new(vec![], mutate, basic_exploration, total);
+    let mempool = CRDT::new(vec![], mutate, mempool_reconciliation, total);
 
     let mut process = Process::new(config.id, &mempool, from_network_chan, &to_network_chan);
     let process_executor = process.execute_chan_sender.clone();
@@ -107,17 +137,19 @@ pub async fn run() {
             });
 
     let execute_task = tokio::spawn(async move {
-        if config.id == 1 {
+        if config.id != 2 {
+            let mut id = config.id as usize;
             loop {
                 tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
-                let op = Operation::new(0, MempoolParameter::Add(Transaction::default()));
+                let op = Operation::new(0, MempoolParameter::Add(Transaction::new(id)));
+                id += config.id as usize;
                 process_executor.send(op).await.unwrap();
             }
         }
         if config.id == 2 {
             loop {
-                tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
-                let op = Operation::new(0, MempoolParameter::Prune());
+                tokio::time::sleep(tokio::time::Duration::from_secs(4)).await;
+                let op = Operation::new(1, MempoolParameter::Prune());
                 process_executor.send(op).await.unwrap();
             }
         }
